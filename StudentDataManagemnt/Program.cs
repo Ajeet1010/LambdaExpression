dc2c1a07f1609b8e241e82a1f5c8cf1255491ef6
Ajeet <ajeetpal101999@gmail.com>
2023-04-06 00:38:22 +0530

    [Ajeet] Fetch Student data using LAMBDA expression.

---
+++ StudentDataManagemnt/Program.cs
@@ -0,0 +1,10 @@
+﻿namespace StudentDataManagemnt
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("*****  Fetch Student Data using LAMBDA Expression  *****");
+        }
+    }
+}
